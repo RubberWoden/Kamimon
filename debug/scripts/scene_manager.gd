7b79487109e6e102543f5d18adfc3cb73c8a1c5c
RubberWoden <41363925+RubberWoden@users.noreply.github.com>
2026-04-08 20:51:12 -0500

    getting help
    upload for help

--- debug/scripts/scene_manager.gd
+++ debug/scripts/scene_manager.gd
@@ -3,6 +3,7 @@ class_name SceneManager extends Node
 
 func change_scene(body, from, to_new_scene: String) -> void:
 	var player = body
-	player.get_parent().remove_child(player)
+	
 	from.get_tree().call_deferred("change_scene_to_file", to_new_scene)	
-	get_parent().add_child(player)
+	var new_scene = ResourceUID.text_to_id(to_new_scene)
+	player.reparent(new_scene)
