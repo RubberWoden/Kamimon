93424325fb23c616b9005530a3bb5307d74f2f06
RubberWoden <41363925+RubberWoden@users.noreply.github.com>
2026-04-08 21:46:49 -0500

    Revert "reverting"
    This reverts commit d68e9cf929838a80d513804176025e8812f6a2c1.

--- debug/scripts/scene_manager.gd
+++ debug/scripts/scene_manager.gd
@@ -1,8 +1,9 @@
 class_name SceneManager extends Node
 
-var player: Player
 
-func change_scene(from, to_new_scene: String) -> void:
-	player = from.player
-	player.get_parent().remove_child(player)
+func change_scene(body, from, to_new_scene: String) -> void:
+	var player = body
+	
 	from.get_tree().call_deferred("change_scene_to_file", to_new_scene)	
+	var new_scene = ResourceUID.text_to_id(to_new_scene)
+	player.reparent(new_scene)
